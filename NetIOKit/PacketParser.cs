0763a1027fad8f174121f829db97cc921d5d778e
MortCat <mortcj0@gmail.com>
2025-05-03 23:50:59 +0800

    加入專案檔案。

---
+++ NetIOKit/PacketParser.cs
@@ -0,0 +1,11 @@
+﻿namespace NetIOKit
+{
+    public class PacketParser
+    {
+        // 1. Treats data as a single packet when a specific header is detected.
+        // 2. Treats data as a single packet when a specific terminator is detected.
+        // 3. Treats data as a single packet when the received length is less than a predefined minimum.
+
+
+    }
+}
